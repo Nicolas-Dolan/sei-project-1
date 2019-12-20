bffbc3e99afe57ba3740214843933d10b695f9f5
Nicolas-Dolan <nicolas.dolan@ymail.com>
2019-12-20 18:12:47 +0000

    added big coins

--- starter-code/scripts/app.js
+++ starter-code/scripts/app.js
@@ -1,13 +1,15 @@
 function init() {
 
+  // GAME VARIABLES
+  let playerIndex = 19
+
   //  DOM VARIABLES
+  const width = 18
   const grid = document.querySelector('.grid')
   const squares = []
-
-  // GAME VARIABLES
-  const width = 18
-  let playerIndex = 19
   const wallIndices = []
+  const bigCoinsIndices = [34, 107, 88]
+  
 
   //FUNCTIONS
 
@@ -65,6 +67,7 @@ function init() {
       }
     }
   }
+
   function drawHorizontalWalls(min, max) {
     let i = min
     for (i = min; i <= max; i++) {
@@ -72,48 +75,67 @@ function init() {
     }
   }
 
-  function drawVerticalWalls(min, max) {
-    let i = min
-    for (i = min; i <= max; i += width) {
-      wallIndices.push(i)
-    }
-  }
-
-  function drawBlock(min, max, n) {
+  function drawBlock(min, max) {
+    const n = Math.ceil((max + 1) / width) - Math.ceil((min + 1) / width) + 1
     let i = 0
     for (i = 0; i < n; i++) {
       drawHorizontalWalls(min + width * (i), max - width * (n - (i + 1)))
     }
   }
 
-
   addPerimeterWalls()
-  drawHorizontalWalls(92, 105)
-  drawVerticalWalls(114, 132)
-  drawBlock(26, 63, 3)
-  drawBlock(38, 60, 2)
-  drawBlock(47, 69, 2)
-  drawHorizontalWalls(127, 130)
-  drawBlock(163, 184, 2)
-  drawBlock(218, 238, 2)
-  drawHorizontalWalls(272, 285)
-  drawVerticalWalls(119, 137)
-  drawBlock(134, 153, 2)
-  drawHorizontalWalls(139, 142)
-  drawBlock(175, 196, 2)
-  drawBlock(168, 186, 2)
-  drawBlock(173, 191, 2)
-  drawBlock(187, 190, 1)
-  drawBlock(206, 225, 2)
-  drawBlock(229, 249, 2)
-  drawBlock(222, 258, 3)
-  drawBlock(227, 263, 3)
-  drawBlock(259, 262, 1)
+  drawBlock(92, 105)
+  drawBlock(114, 132)
+  drawBlock(26, 63)
+  drawBlock(38, 60)
+  drawBlock(47, 69)
+  drawBlock(127, 130)
+  drawBlock(163, 184)
+  drawBlock(218, 238)
+  drawBlock(272, 285)
+  drawBlock(119, 137)
+  drawBlock(134, 153)
+  drawBlock(139, 142)
+  drawBlock(175, 196)
+  drawBlock(168, 186)
+  drawBlock(173, 191)
+  drawBlock(187, 190)
+  drawBlock(206, 225)
+  drawBlock(229, 249)
+  drawBlock(222, 258)
+  drawBlock(227, 263)
+  drawBlock(259, 262)
+
+  // function placeBigCoins() {
+
+  // }
+
+  // function placeSmallCoins
+
 
   console.log('wallIndices', wallIndices)
 
   // adds walls to grid
   wallIndices.forEach((element) => squares[element].classList.add('wall'))
+
+  //adds big coins to grid
+  bigCoinsIndices.forEach((element) => {
+    if (!squares[element].classList.contains('wall')) {
+      squares[element].classList.add('bigCoin')
+    }
+  })
+
+  //adds small coins to grid
+  // function addSmallCoins() {
+  //   squares.forEach((element) => {
+  //     if (!squares[element].classList.contains('wall') && !squares[element].classList.contains('player') && !squares[element].classList.contains('bigCoin')) {
+  //       squares[element].classList.add('smallCoin')
+  //     }
+  //   }) 
+  // }
+  // addSmallCoins()
+
+  
 
   // adds index number to each grid item
   squares.forEach((element, index) => element.innerHTML = index)
